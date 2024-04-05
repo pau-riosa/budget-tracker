2558aa40e08f3c3a69dc4967664db1b0cb775b06
pau-riosa <jethro.riosa@gmail.com>
2024-04-05 08:37:40 +0800

    create debts

---
+++ lib/budget_tracker/debts/debt.ex
@@ -0,0 +1,19 @@
+defmodule BudgetTracker.Debts.Debt do
+  use BudgetTracker.Schema
+
+  schema "debts" do
+    field :debt_type, :string
+    field :total_amount, :float
+    field :monthly_payment_amount, :float
+    belongs_to :user, BudgetTracker.Accounts.User
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(debt, attrs) do
+    debt
+    |> cast(attrs, [:debt_type, :total_amount, :monthly_payment_amount, :user_id])
+    |> validate_required([:debt_type, :total_amount, :monthly_payment_amount, :user_id])
+  end
+end
