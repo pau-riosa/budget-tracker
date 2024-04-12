82ac0d28655a544d93e0e559101d4520c04a9e8c
pau-riosa <jethro.riosa@gmail.com>
2024-04-12 09:26:16 +0800

    add precision

--- lib/budget_tracker_web/live/budget_setting_live/form_component.ex
+++ lib/budget_tracker_web/live/budget_setting_live/form_component.ex
@@ -27,7 +27,7 @@ defmodule BudgetTrackerWeb.BudgetSettingLive.FormComponent do
           options={Ecto.Enum.values(BudgetTracker.BudgetSettings.BudgetSetting, :category)}
         />
         <.input field={@form[:name]} type="text" label="Name" />
-        <.input field={@form[:planned_amount]} type="number" label="Planned amount" step="any" />
+        <.input field={@form[:planned_amount]} type="number" label="Planned amount" step="0.01" />
         <.input field={@form[:user_id]} type="hidden" value={@current_user.id} />
         <:actions>
           <.button phx-disable-with="Saving...">Save Budget setting</.button>
