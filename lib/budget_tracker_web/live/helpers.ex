1be07b29e3ea3cc6105dc0bb4dabfb88fa4a2e78
pau-riosa <jethro.riosa@gmail.com>
2024-04-17 08:09:59 +0800

    applied changes

--- lib/budget_tracker_web/live/helpers.ex
+++ lib/budget_tracker_web/live/helpers.ex
@@ -3,5 +3,9 @@ defmodule BudgetTrackerWeb.Live.Helpers do
   Functions and LiveView helpers for the application.
   """
 
+  @spec format_month_year(DateTime.t()) :: String.t()
+  def format_month_year(datetime), do: Calendar.strftime(datetime, "%B %Y")
+
+  @spec format_datetime(DateTime.t()) :: String.t()
   def format_datetime(datetime), do: Calendar.strftime(datetime, "%B %d %Y")
 end
